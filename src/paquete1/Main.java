ddd60f735a37aa2253baa1e9351792bf46e0574b
mrStark-MK3 <dennis_rodriguez@unitec.edu>
2025-03-10 02:13:30 -0600

    Parte2_Proyect

--- src/paquete1/Main.java
+++ src/paquete1/Main.java
@@ -29,43 +29,38 @@ public class Main {
                     break;
                 case 2:
                     System.out.println("REGISTRARSE");
+                    
                     // Proceso de registro
                     System.out.println("Rellene los campos obligatorios");
+                    System.out.println("------------------------------------");
+                    
+                    // Ingresar nombre
                     System.out.print("Ingresa tu nombre: ");
                     input.nextLine();
                     String nombre = input.nextLine();
+                    
+                    // Ingresar ID
                     System.out.print("Ingresa tu ID: ");
                     String id = input.nextLine();
+                    
+                    // ingresar edad
                     System.out.print("Ingresa tu edad: ");
                     int edad = input.nextInt();
-                    System.out.print("Elije tu tipo de plan");
-                    System.out.println("1. Premium - L. 600");
-                    System.out.println("2. Normal - L. 500");
-                    System.out.print("Ingresa el e numero de la opcion: ");
-                    int plan = input.nextInt();
-                    // Validar que el usuario ingrese una opcion valida
-                    while (plan < 1 || plan > 2) {
-                        System.out.println("Opcion no valida");
-                        System.out.println("Vuelva a intentarlo");
-                        System.out.print("-> ");
-                        plan = input.nextInt();
+                    if (edad < 14) {
+                        break;
                     }
-                    String tipo_de_cliente = "";
-                    // Finalizar de crear el objeto tipo Usuario dependiendo el plan
-                    switch (plan) {
-                        case 1:
-                            tipo_de_cliente = "Premium";
-                            break;
-                        case 2:
-                            tipo_de_cliente = "Normal";
-                            break;
-                    }
+                    
+                    // Elegir el tipo de plan
+                    System.out.println("Elije tu tipo de plan");
+                    int plan = md.TipoDePlan();
+                    String tipo_de_cliente = md.TipoDeCliente(plan);
+                    
                     // Crear contrasenia
                     System.out.println("Crea tu contrasenia");
                     System.out.print("-> ");
                     String password = input.nextLine();
                     // Validar contrasenia
-                    
+                    password = md.ValidarPassword(password);
                     System.out.println("Confirmar contrasenia");
                     System.out.print("-> ");
                     String confirm = input.nextLine();
@@ -75,21 +70,33 @@ public class Main {
                         System.out.println("-> ");
                         confirm = input.nextLine();
                     }
+                    
                     // Guardar usuario en el array de Usuarios
                     array[usuariosCreados] = new Usuario(nombre, id, edad, tipo_de_cliente, password);
                     usuariosCreados ++;
-                    // Usuario registrado
                     break;
                 case 3:
-                    // Solicitar el nombre
+                    // Solicitar datos de confirmacion
                     System.out.print("Ingresa tu nombre: ");
+                    nombre = input.nextLine();
+                    System.out.print("Ingresa tu ID: ");
+                    id = input.nextLine();
                     // Validar si existe
-                    //------------
-                    System.out.println("Confirmar");
-                    System.out.print("SI/NO -> ");
-                    char eliminar = input.nextLine().charAt(0);
-                    if (eliminar == 's' || eliminar == 'S') {
-                        array[elegido] = null;
+                    boolean existe = md.BuscarUsuario(nombre, id, array);
+                    if (existe) {
+                        // Confirmar con la contrasenia
+                        password = input.nextLine();
+                        int posicionEnArray = md.PosicionEnArray(nombre, id, array);
+                        boolean confirmarPassword = md.ComprobarPassword(password, posicionEnArray, array);
+                        
+                        if (confirmarPassword) {
+                            System.out.println("Confirmar");
+                            System.out.print("SI/NO -> ");
+                            char eliminar = input.nextLine().charAt(0);
+                            if (eliminar == 's' || eliminar == 'S') {
+                                array[usuariosCreados] = null;
+                            }   
+                        }
                     }
                     break;
                 case 4:
--- src/paquete1/Metodos.java
+++ src/paquete1/Metodos.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Metodos {
     
+    // Metodos de registro
     public Usuario [] Arreglo() {
         
         // Crear arreglo de tipo Usuario
@@ -11,5 +12,136 @@ public class Metodos {
         
         return array;
     }
+    public String ValidarPassword(String password) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        boolean valida = true;
+        do {
+            int mayus = 0, minus = 0, nums = 0, especial = 0;
+            if (password.length() >= 8) {
+                for (int i = 0; i < password.length(); i++) {
+                    if (password.charAt(i) > 64 && password.charAt(i) < 91) {
+                        mayus ++;
+                    } else if (password.length() > 96 && password.charAt(i) < 123) {
+                        minus ++;
+                    } else if (password.charAt(i) > 47 && password.charAt(i) < 58) {
+                        nums ++;
+                    } else {
+                        especial ++;
+                    }
+                }
+                if (mayus > 0 && minus > 0 && nums > 0 && especial > 0) {
+                    break;
+                } else {
+                    System.out.println("La contrasenia debe tener mayusculas, minusculas, numero y caracteres especiales");
+                    valida = false;
+                }
+            } else {
+                System.out.println("La contrasenia debe tener por lo menos 8 caracteres");
+                valida = false;
+            }
+            System.out.println("Intenta con una nueva");
+            System.out.print("-> ");
+            password = input.nextLine();
+        } while (!valida);
+        
+        return password;
+    }
+    public int TipoDePlan() {
+        
+        Scanner input = new Scanner(System.in);
+        
+        int plan;
+        char confirmPlan = 0;
+        do {
+            System.out.println("1. Premium - L. 600");
+            System.out.println("2. Normal - L. 500");
+            System.out.print("Ingresa el e numero de la opcion: ");
+            plan = input.nextInt();
+            // Validar que el usuario ingrese una opcion valida
+            while (plan < 1 || plan > 2) {
+                System.out.println("Opcion no valida");
+                System.out.println("Vuelva a intentarlo");
+                System.out.print("-> ");
+                plan = input.nextInt();
+            }
+            switch (plan) {
+                case 1:
+                    System.out.println("Confirma que desea usar el plan Premium?");
+                    confirmPlan = input.nextLine().charAt(0);
+                    break;
+                case 2:
+                    System.out.println("Confirma que desea usar el plan Premium?");
+                    confirmPlan = input.nextLine().charAt(0);
+                    break;
+            }
+        } while (confirmPlan == 'n' || confirmPlan == 'N');
+
+        return plan;
+    }
+    public String TipoDeCliente(int plan) {
+        
+        String tipo_de_cliente = "";
+        // Agregar tipo de plan al objeto Usuario
+        switch (plan) {
+            case 1:
+                tipo_de_cliente = "Premium";
+                break;
+            case 2:
+                tipo_de_cliente = "Normal";
+                break;
+        }
+        
+        return tipo_de_cliente;
+    }
     
+    // Metodos de eliminacion/acceso
+    public boolean BuscarUsuario(String nombre, String id, Usuario [] array) {
+        
+        boolean existe = false;
+        
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].getNombre().equalsIgnoreCase(nombre) && array[i].getId().equals(id)) {
+                existe = true;
+                break;
+            }
+        }
+        
+        return existe;
+    }
+    public int PosicionEnArray(String nombre, String id, Usuario [] array) {
+        
+        int posicion = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].getNombre().equalsIgnoreCase(nombre) && array[i].getId().equals(id)) {
+                posicion = i;
+                break;
+            }
+        }
+        
+        return posicion;    
+    }
+    public boolean ComprobarPassword(String password, int posicionEnArray, Usuario [] array) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        boolean confirmado = true;
+        int cont = 0;
+        while (cont < 3) {
+            if (!array[posicionEnArray].getPassword().equals(password)) {
+                confirmado = false;
+            } else {
+                confirmado = true;
+                break;
+            }
+            System.out.println("Contrasenia incorrecta");
+            System.out.println("Intentalo de nuevo");
+            System.out.print("-> ");
+            password = input.nextLine();
+        }
+        
+        return confirmado;
+    }
 }
